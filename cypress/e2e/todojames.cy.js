ee7ed6978124bdc8913ad6ecf27cd450747edbdf
Vaidul1s <vaidas.ad25@gmail.com>
2025-02-12 15:17:32 +0200

    02.12

---
+++ cypress/e2e/todojames.cy.js
@@ -0,0 +1,26 @@
+// Susikurti naują GitHub projektą
+// Jame susidiegti Cypress
+// Parašyti testus paskaitoje naudojamai svetainei https://todolist.james.am/#/ :
+//     3.1. Ar <header> elementas atvaizduojamas.
+//     3.2. Ar headeryje atvaizduojamas tekstas "To Do List"
+//     3.3. Ar atvaizduojamas 'Double-click to edit a todo' tekstas
+//     3.4 Ar input laukelyje atvaizduojamas tekstas 'What need's to be done?'
+//     3.5. Ar pridėjus kelias užduotis, užduočių sąrašas nėra tuščias
+
+describe('ToDo Simple Test', () => {
+
+    it('visit ToDo site', () => {
+        cy.visit('https://todolist.james.am/#/');
+        cy.get('header').should('be.visible');
+        cy.get('h1').should('be.visible');
+        cy.get('footer p').should('be.visible').and('have.text', 'Double-click to edit a toodo'); // tinklapyje gramatine klaida frazėje "... toodo" vietoj "todo"!
+        cy.get('input.new-todo').should('be.visible').and('have.attr', 'placeholder', `What need's to be done?`);
+
+        ['first entry', 'second entry', 'fourth entry'].forEach(e => {cy.get('input.new-todo').type(`${e}{enter}`)});
+        cy.get('ul.todo-list li').should('have.length', 3);
+
+    });
+
+   
+
+});
---
+++ server.js
@@ -0,0 +1,146 @@
+const express = require('express');
+const app = express();
+const pool = require('./database');
+
+const port = 5555;
+
+app.use(express.json());
+
+
+
+//-----------------------------Routes-----------------------------
+
+app.get('/users', async (req, res) => {
+
+    try {
+        const results = await pool.query(`SELECT * FROM users`);
+        res.status(200).json(results.rows);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.get('/users/:id', async (req, res) => {
+
+    try {
+        const id = req.params.id
+        const results = await pool.query(`SELECT * FROM users WHERE id=${id}`);
+        res.status(200).json(results.rows);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.post('/users', async (req, res) => {
+
+    try {
+        const { id, username, password } = req.body;
+
+        const results = await pool.query(`INSERT INTO users (id,username, password) VALUES (${id}, '${username}', '${password}') RETURNING *`);
+        res.status(201).json(results.rows[0]);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.put('/users/:id', async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const { username, password } = req.body;
+        const results = await pool.query(`UPDATE users SET username='${username}', password='${password}' WHERE id=${id} RETURNING *`);
+        res.status(200).json(results.rows[0]);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.delete('/users/:id', async (req, res) => {
+
+    try {
+        const id = req.params.id
+        const results = await pool.query(`DELETE FROM users WHERE id=${id} RETURNING *`);
+        res.status(200).json(results.rows);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.get('/products', async (req, res) => {
+
+    try {
+        const results = await pool.query(`SELECT * FROM products`);
+        res.status(200).json(results.rows);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.get('/products/:id', async (req, res) => {
+
+    try {
+        const id = req.params.id
+        const results = await pool.query(`SELECT * FROM products WHERE id=${id}`);
+        res.status(200).json(results.rows[0]);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.post('/products', async (req, res) => {
+
+    try {
+        const { name, price, description } = req.body;
+
+        const results = await pool.query(`INSERT INTO products (name, price, description) VALUES ('${name}', ${price}, '${description}') RETURNING *`);
+        res.status(201).json(results.rows[0]);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.put('/products/:id', async (req, res) => {
+
+    try {
+        const id = req.params.id
+        const { name, price, description } = req.body;
+        const results = await pool.query(`UPDATE products SET name='${name}', price='${price}', description='${description}' WHERE id=${id} RETURNING *`);
+        res.status(200).json(results.rows[0]);
+
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.delete('/products/:id', async (req, res) => {
+
+    try {
+        const id = req.params.id
+        const results = await pool.query(`DELETE FROM products WHERE id=${id} RETURNING *`);
+        res.status(200).json({message: 'Produktas ištrintas sėkmingai!'});             
+    } catch (error) {
+        res.status(400).json({ error: 'error' });
+    }
+
+});
+
+app.listen(port, () => {
+    console.log(`CRUD API darbui pasiruošus ant ${port} porto!`)
+});
